e90cba1eb2d7aa46359900ca8f569640b06e29de
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-03-01 20:34:55 +0100

    css home page still.../tag search / login form

--- src/components/QuickTagSearch.jsx
+++ src/components/QuickTagSearch.jsx
@@ -4,28 +4,43 @@ import APIHandler from './../api/APIHandler';
 
 export default class QuickTagSearch extends Component {
     state = {
-        tags: []
+        isToggle: false,
+        tags: [],
+        selectedTags: [],
+        selected: "not-selected"
+    }
+
+    handleClick = e => {
+        console.log(e.target.value)
+        this.setState({isToggle: !this.state.isToggle})
+        if (this.state.isToggle) {
+            e.target.className = "selected" 
+        } else {
+            e.target.className = "not-selected"
+        }
     }
 
     componentDidMount() {
         APIHandler.get("/tags")
         .then(apiRes => {
-            console.log(apiRes.data);
-            this.setState({tags: apiRes.data})
+            this.setState({tags: apiRes.data.apiRes})
         })
         .catch(apiErr => console.error(apiErr));
     };
     
     render() {
         return (
-            <div>
+            <>
                 <h2>Quick search</h2>
                 <div className="tag-list">
                     <ul>
-                        
+                        {this.state.tags.map((tag, i) => (
+                        <li className={this.state.selected} onClick={this.handleClick} key={i} value={tag.name}>{tag.name}</li>
+                        ))}
                     </ul>
                 </div>
-            </div>
+                <button className="btn-tag">QUICK SEARCH</button>
+            </>
         )
     }
 }
