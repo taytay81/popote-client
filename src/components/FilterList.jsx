91b8bfbb3beaa53ae4d65e6987aad2903981c5bc
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-03-01 13:00:19 +0100

    Home page. Set up to display the filter's page + quick search by tags

---
+++ src/components/FilterList.jsx
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react'
+// import APIHandler from './../api/APIHandler'
+
+// export default class extends Component {
+//     state = {
+//         tags: []
+//     };
+
+//     componentDidMount() {
+//         APIHandler.get("/")
+//         .then(apiRes => {
+//             this.setState({tags: apiRes})
+//         })
+//         .catch(apiErr => console.error(apiErr));
+//     };
+
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import NavBar from "./../components/NavBar";
 import '../styles/home.css';
+// import FilterList from "./../components/FilterList";
+import QuickTagSearch from "./../components/QuickTagSearch";
 
 
 export default class Home extends Component {
@@ -14,7 +16,36 @@ export default class Home extends Component {
                         <h2 id="decale">we will find something to eat for you...</h2>
                     </div>
                     <div className="arrow-down">
-                        <a href=""><span className="arrow">v</span></a>
+                        <a href="#"><span className="arrow">v</span></a>
+                    </div>
+                </div>
+
+                <div className="home-second-page">
+                    <div className="container">
+                        <div className="marketing-question">
+                            <span>Which ingredient do you have ?</span>
+                        </div>
+                        <form>
+                            <div className="search-bar">
+                                <div className="input-and-btn">
+                                    <input id="main-search-bar" name="ingredient" 
+                                    placeholder="Ex : tomato, peach..." type="text"/>
+                                    <button className="btn-search">SEARCH</button>
+                                </div>
+                                
+                                <div className="more-filter">
+                                    <a href="#"><span id="style-span">more filters</span></a>
+                                </div>
+                                
+                            </div>
+                        </form>
+
+                        <div className="filter-list">
+                            {/* <FilterList/> */}
+                        </div>
+                        <div className="quick-tag-search">
+                            <QuickTagSearch/>
+                        </div>
                     </div>
                 </div>
             </div>
