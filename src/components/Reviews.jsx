da99b0cd5aa969d5d7236416aa1aa82e4066e9e8
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-03-06 11:08:43 +0100

    working on the stars

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,19 +1,31 @@
 import React, { Component, useContext, useState } from "react";
 import "./../styles/rating.css";
 import StarWrapper from "./StarWrapper";
+import { useAuth } from "../auth/useAuth";
+import UserContext from "../auth/UserContext";
 
 export default function Reviews({ reviews }) {
+  useAuth();
+  const { currentUser } = useContext(UserContext);
+
   return (
     <div className="right-side">
       <ul>
         {reviews.length ? (
           reviews.map((r, i) => (
             <li key={i}>
-              <div className="info-user-review">
-                <div className="rating-area">
-                  <StarWrapper rating={r.rating} clickable={false} />
+              <div className="box-review">
+                <div className="info-user-review">
+                  <div className="name-area">
+                    {currentUser.firstname} {currentUser.lastname}
+                  </div>
+                  <div className="rating-area">
+                    <StarWrapper rating={r.rating} clickable={false} />
+                  </div>
                 </div>
-                <div className="comment">{r.body}</div>
+                <div className="comment">
+                  <div>{r.body}</div>
+                </div>
               </div>
             </li>
           ))
